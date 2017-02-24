e55ffdb5e0ec64925f3579eda8b9987d78aff0d3
Reicher <robin.o.reicher@gmail.com>
2017-02-24 19:20:46 +0100

    fixed stuff

--- src/game.js
+++ src/game.js
@@ -6,52 +6,52 @@ import kkkMeeting from './kkkmeeting'
 import ChurchMeeting from './churchmeeting'
 
 export default class extends Phaser.State {
-  init () {}
+    init () {}
 
-  preload () {}
+    preload () {}
 
-  create () {
+    create () {
 
-    this.trump = new Trump(this.game)
+	this.trump = new Trump(this.game)
 
-    // 1. Create list with all corridors
-    // 2. Run lobby scene
-    // 3. Pick a corridor
-    // 4. Start corridor scene
-    // 5. Wait for corridor scene to complete
-    // 6. Pick new corridor
-    // 7. IF more corridors Goto 4 ELSE Goto 8
-    // 8. Run final scene
+	// 1. Create list with all corridors
+	// 2. Run lobby scene
+	// 3. Pick a corridor
+	// 4. Start corridor scene
+	// 5. Wait for corridor scene to complete
+	// 6. Pick new corridor
+	// 7. IF more corridors Goto 4 ELSE Goto 8
+	// 8. Run final scene
 
-    this.levels = []
-    //this.levels.push(new kkkMeeting(this.game, this.trump))
-    //this.levels.push(new ChurchMeeting(this.game, this.trump))
-    this.levels.push(new PrideParty(this.game, this.trump))
-    this.nextLevel()
-  }
-
-  nextLevel () {
-    if (this.levels.length > 0) {
-      this.level = this.levels.pop()
-
-      this.level.onLevelComplete.addOnce(()=>{
-        this.nextLevel()
-      })
-
-      this.level.start()
+	this.levels = []
+	this.levels.push(new kkkMeeting(this.game, this.trump))
+	//this.levels.push(new ChurchMeeting(this.game, this.trump))
+	this.levels.push(new PrideParty(this.game, this.trump))
+	this.nextLevel()
     }
-    else {
-        this.state.start('GameOverMenu')
+
+    nextLevel () {
+	if (this.levels.length > 0) {
+	    this.level = this.levels.pop()
+
+	    this.level.onLevelComplete.addOnce(()=>{
+		this.nextLevel()
+	    })
+
+	    this.level.start()
+	}
+	else {
+            this.state.start('GameOverMenu')
+	}
     }
-  }
 
-  update () {
-    if(this.level)
-      this.level.update();
-  }
+    update () {
+	if(this.level)
+	    this.level.update();
+    }
 
-  render () {
-//    if (__DEV__) {
-//    }
-  }
+    render () {
+	//    if (__DEV__) {
+	//    }
+    }
 }
