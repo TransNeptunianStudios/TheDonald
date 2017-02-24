b16cefe6eeb591a7ed085b80b125e4c2b7adbf2b
Robin Reicher <Robin.Reicher@westermo.se>
2017-02-24 17:06:53 +0100

    Added new elevator door and trump death!!

--- src/splash.js
+++ src/splash.js
@@ -1,58 +1,59 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
-  init () {
-    this.stage.backgroundColor = '#000000'
-  }
+    init () {
+	this.stage.backgroundColor = '#000000'
+    }
 
-  preload () {
-    // Should these really be here?
-    this.load.image('tnsLogo', './assets/images/tns-logo.png')
-    this.load.image('playBtn', './assets/images/play-button.png')
-    this.load.image('menuBackground', './assets/images/menu-background.png')
-    this.load.image('sky', './assets/images/sky.png')
-    this.load.image('baseCorridor', './assets/images/base-corridor.png')
-    this.load.image('prideCorridor', './assets/images/pride-corridor.png')
-    this.load.image('churchCorridor', './assets/images/church-corridor.png')
-    this.load.image('kkkCorridor', './assets/images/kkk-corridor.png')
-    this.load.image('office', './assets/images/office.png')
-    this.load.spritesheet('cross', './assets/images/KKK-Kors.png', 126, 208)
-    this.load.image('kkk_ladder', './assets/images/kkk_stege.png')
-    this.load.image('kkk_seller', './assets/images/kkk_seller.png')
-    this.load.spritesheet('clansman', './assets/images/clansman.png', 80, 123)
+    preload () {
+	// Should these really be here?
+	this.load.image('tnsLogo', './assets/images/tns-logo.png')
+	this.load.image('playBtn', './assets/images/play-button.png')
+	this.load.image('menuBackground', './assets/images/menu-background.png')
+	this.load.image('sky', './assets/images/sky.png')
+	this.load.image('baseCorridor', './assets/images/base-corridor.png')
+	this.load.image('prideCorridor', './assets/images/pride-corridor.png')
+	this.load.image('churchCorridor', './assets/images/church-corridor.png')
+	this.load.image('kkkCorridor', './assets/images/kkk-corridor.png')
+	this.load.image('office', './assets/images/office.png')
+	this.load.spritesheet('cross', './assets/images/KKK-Kors.png', 126, 208)
+	this.load.image('kkk_ladder', './assets/images/kkk_stege.png')
+	this.load.image('kkk_seller', './assets/images/kkk_seller.png')
+	this.load.spritesheet('clansman', './assets/images/clansman.png', 80, 123)
 
-    this.load.image('elevator', './assets/images/elevator.png')
-    this.load.image('elevator-door', './assets/images/elevator-door.png')
-    this.load.image('elevator-button', './assets/images/elevator-button.png')
+	this.load.image('elevator', './assets/images/elevator.png')
+	this.load.image('elevator-door', './assets/images/elevator-door.png')
+	this.load.image('elevator-button', './assets/images/elevator-button.png')
 
-    this.load.spritesheet('trump', './assets/images/trump.png', 50, 100)
-    this.load.spritesheet('opponent', './assets/images/vickan.png', 17, 32)
+	this.load.spritesheet('trump', './assets/images/trump.png', 50, 100)
+        this.load.spritesheet('trump_game_over', './assets/images/trump_death.png', 50, 150)
+	this.load.spritesheet('opponent', './assets/images/vickan.png', 17, 32)
 
-    this.load.audio('suffer', './assets/sounds/i_just_want_them_to_suffer.mp3')
-    this.load.audio('noAction', './assets/sounds/too_much_talk_not_enough_action.mp3')
-    this.load.audio('elevatorBing', './assets/sounds/elevator_bing.mp3')
-  }
+	this.load.audio('suffer', './assets/sounds/i_just_want_them_to_suffer.mp3')
+	this.load.audio('noAction', './assets/sounds/too_much_talk_not_enough_action.mp3')
+	this.load.audio('elevatorBing', './assets/sounds/elevator_bing.mp3')
+    }
 
-  create () {
-    this.logo = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'tnsLogo')
-    this.logo.anchor.setTo(0.5)
-    this.logo.alpha = 0
+    create () {
+	this.logo = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'tnsLogo')
+	this.logo.anchor.setTo(0.5)
+	this.logo.alpha = 0
 
-    // Start fade in of TNS logo
-    let fadeInTween = this.game.add.tween(this.logo).to({alpha: 1}, 2000, Phaser.Easing.Linear.None, true)
-    fadeInTween.onComplete.add(()=>{
-      // Wait one second with full alpha
-      this.game.time.events.add(Phaser.Timer.SECOND * 1, ()=>{
-        // Start to fade out
-        let fadeOutTween = this.game.add.tween(this.logo).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true)
-        fadeOutTween.onComplete.add(()=>{
-          this.state.start('MainMenu')
-        }, this)
-      }, this)
-    })
+	// Start fade in of TNS logo
+	let fadeInTween = this.game.add.tween(this.logo).to({alpha: 1}, 2000, Phaser.Easing.Linear.None, true)
+	fadeInTween.onComplete.add(()=>{
+	    // Wait one second with full alpha
+	    this.game.time.events.add(Phaser.Timer.SECOND * 1, ()=>{
+		// Start to fade out
+		let fadeOutTween = this.game.add.tween(this.logo).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true)
+		fadeOutTween.onComplete.add(()=>{
+		    this.state.start('MainMenu')
+		}, this)
+	    }, this)
+	})
 
-    this.game.input.onDown.add(()=>{
-          this.state.start('MainMenu')
-    }, this)
-  }
+	this.game.input.onDown.add(()=>{
+            this.state.start('MainMenu')
+	}, this)
+    }
 }
--- src/trump.js
+++ src/trump.js
@@ -3,113 +3,125 @@ import Quote from './quote'
 import HealthBar from './healthbar'
 
 export default class Trump extends Phaser.Sprite {
-  constructor(game) {
-    super(game, 0, 0, 'trump')
-    this.anchor.setTo(0.5, 1);
+    constructor(game) {
+	super(game, 0, 0, 'trump')
+	this.anchor.setTo(0.5, 1);
 
-    this.frame = 2;
-    this.animations.add('north', [4, 5, 6, 7, 8, 9, 10, 11], 15, true);
-    this.animations.add('west', [12, 13, 14, 15, 16, 17, 18, 19], 15, true);
-    this.animations.add('south', [20, 21, 22, 23, 24, 25, 26, 27], 15, true);
-    this.animations.add('east', [28, 29, 30, 31, 32, 33, 34, 35], 15, true);
+	this.frame = 2;
+	this.animations.add('north', [4, 5, 6, 7, 8, 9, 10, 11], 15, true);
+	this.animations.add('west', [12, 13, 14, 15, 16, 17, 18, 19], 15, true);
+	this.animations.add('south', [20, 21, 22, 23, 24, 25, 26, 27], 15, true);
+	this.animations.add('east', [28, 29, 30, 31, 32, 33, 34, 35], 15, true);
 
-    this.onCallingElevator = new Phaser.Signal()
-    this.onReadyForDebate = new Phaser.Signal()
-    this.walkTween = this.game.add.tween(this);
+	this.onCallingElevator = new Phaser.Signal()
+	this.onReadyForDebate = new Phaser.Signal()
+	this.walkTween = this.game.add.tween(this);
 
-    this.healthbar = new HealthBar(this.game, {x: this.game.world.centerX, y: 50})
+	this.healthbar = new HealthBar(this.game, {x: this.game.world.centerX, y: 50})
 
-    this.confidence = 100
+	this.confidence = 100
 
-    this.createQuotes()
-  }
+	this.createQuotes()
+    }
 
-  // Initialize Trump for a new level
-  initLevel(point) {
-    this.frame = 2
-    this.position.setTo(point.x, point.y - 10)
-    this.healthbar.draw()
-  }
+    game_over(){
+	this.loadTexture('trump_game_over', 0);
+	this.animations.add('explode');
+	this.animations.add('fissle', [33, 34])
+	this.animations.play('explode', 10);
 
-  // walks trump in a direction with the animation running
-  walkDirection(dx, dy){
-    let time = Math.max(Math.abs(dx), Math.abs(dy))*7.5;
+	this.events.onAnimationComplete.add(function(){
+	    this.play('fissle', 10, true)
+	}, this);
+    }
 
-    this.walkTween = this.game.add.tween(this).to({x: this.x+dx,y: this.y+dy}, time, Phaser.Easing.Linear.None, true);
-    this.walkTween.onComplete.add(()=>{this.animations.stop()}, this)
+    // Initialize Trump for a new level
+    initLevel(point) {
+	this.frame = 2
+	this.position.setTo(point.x, point.y - 10)
+	this.healthbar.draw()
+    }
 
-    if( dx == 0 && dy == 0 ) this.animations.stop()
-    else if( dx > 0 ) this.animations.play('east')
-    else if( dy > 0 ) this.animations.play('south')
-    else if( dx < 0 ) this.animations.play('west')
-    else if( dy < 0 ) this.animations.play('north')
+    // walks trump in a direction with the animation running
+    walkDirection(dx, dy){
+	let time = Math.max(Math.abs(dx), Math.abs(dy))*7.5;
 
-    return this.walkTween;
-  }
+	this.walkTween = this.game.add.tween(this).to({x: this.x+dx,y: this.y+dy}, time, Phaser.Easing.Linear.None, true);
+	this.walkTween.onComplete.add(()=>{this.animations.stop()}, this)
 
-  createQuotes () {
-    this.quotes = []
+	if( dx == 0 && dy == 0 ) this.animations.stop()
+	else if( dx > 0 ) this.animations.play('east')
+	else if( dy > 0 ) this.animations.play('south')
+	else if( dx < 0 ) this.animations.play('west')
+	else if( dy < 0 ) this.animations.play('north')
 
-    let suffer = new Quote(game, 'suffer')
-    suffer.addWord('i', 0.0, 0.35)
-    suffer.addWord('just', 0.35, 0.30)
-    suffer.addWord('want', 0.65, 0.25)
-    suffer.addWord('them', 0.90, 0.20)
-    suffer.addWord('to', 1.10, 0.25)
-    suffer.addWord('suffer', 1.35, 0.65)
-    this.quotes.push(suffer)
+	return this.walkTween;
+    }
 
-    let noAction = new Quote(game, 'noAction')
-    noAction.addWord('to', 0.0, 0.25)
-    noAction.addWord('much', 0.20, 0.35)
-    noAction.addWord('talk', 0.6, 0.5)
-    noAction.addWord('not', 1.30, 0.22)
-    noAction.addWord('enough', 1.55, 0.20)
-    noAction.addWord('action', 1.75, 0.4)
-    this.quotes.push(noAction)
-  }
+    createQuotes () {
+	this.quotes = []
 
-  getQuotes (difficulty) {
-    // TODO: Add more quotes and sort with difficulties
-    return this.quotes
-  }
+	let suffer = new Quote(game, 'suffer')
+	suffer.addWord('i', 0.0, 0.35)
+	suffer.addWord('just', 0.35, 0.30)
+	suffer.addWord('want', 0.65, 0.25)
+	suffer.addWord('them', 0.90, 0.20)
+	suffer.addWord('to', 1.10, 0.25)
+	suffer.addWord('suffer', 1.35, 0.65)
+	this.quotes.push(suffer)
 
-  incrementConfidence () {
-    this.confidence += 10
-    this.healthbar.setPercent(this.confidence);
-  }
+	let noAction = new Quote(game, 'noAction')
+	noAction.addWord('to', 0.0, 0.25)
+	noAction.addWord('much', 0.20, 0.35)
+	noAction.addWord('talk', 0.6, 0.5)
+	noAction.addWord('not', 1.30, 0.22)
+	noAction.addWord('enough', 1.55, 0.20)
+	noAction.addWord('action', 1.75, 0.4)
+	this.quotes.push(noAction)
+    }
 
-  decrementConfidence () {
-    this.confidence -= 10
-    this.healthbar.setPercent(this.confidence);
-  }
+    getQuotes (difficulty) {
+	// TODO: Add more quotes and sort with difficulties
+	return this.quotes
+    }
 
-  // walks from elevator to elevator
-  doFullWalk() {
-    this.walkDirection(0, 120).onComplete.addOnce(()=>{
-        this.walkDirection(690, 0).onComplete.addOnce(()=>{
-          this.walkDirection(0, -90).onComplete.addOnce(()=>{
-            this.onCallingElevator.dispatch();
-          }, this)
-        }, this)
-    }, this)
-  }
+    incrementConfidence () {
+	this.confidence += 10
+	this.healthbar.setPercent(this.confidence);
+    }
 
-  // walks from elevator to level middle
-  doDebateWalk() {
-    this.walkDirection(0, 120).onComplete.addOnce(()=>{
-      this.walkDirection(345, 0).onComplete.addOnce(()=>{
-        this.onReadyForDebate.dispatch();
-      }, this)
-    }, this)
-  }
+    decrementConfidence () {
+	this.confidence -= 10
+	this.healthbar.setPercent(this.confidence);
+    }
 
-  // walks from middle to end elevator
-  doRestOfWalk() {
-    this.walkDirection(345, 0).onComplete.addOnce(()=>{
-      this.walkDirection(0, -90).onComplete.addOnce(()=>{
-        this.onCallingElevator.dispatch();
-      }, this)
-    }, this)
-  }
+    // walks from elevator to elevator
+    doFullWalk() {
+	this.walkDirection(0, 120).onComplete.addOnce(()=>{
+	    this.walkDirection(690, 0).onComplete.addOnce(()=>{
+		this.walkDirection(0, -90).onComplete.addOnce(()=>{
+		    this.onCallingElevator.dispatch();
+		}, this)
+	    }, this)
+	}, this)
+    }
+
+    // walks from elevator to level middle
+    doDebateWalk() {
+	this.walkDirection(0, 120).onComplete.addOnce(()=>{
+	    this.walkDirection(345, 0).onComplete.addOnce(()=>{
+		this.game_over()
+		//this.onReadyForDebate.dispatch();
+	    }, this)
+	}, this)
+    }
+
+    // walks from middle to end elevator
+    doRestOfWalk() {
+	this.walkDirection(345, 0).onComplete.addOnce(()=>{
+	    this.walkDirection(0, -90).onComplete.addOnce(()=>{
+		this.onCallingElevator.dispatch();
+	    }, this)
+	}, this)
+    }
 }
