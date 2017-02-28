7fea7a8766f78abf268b6576acc7d09eabe542cd
Reicher <robin.o.reicher@gmail.com>
2017-02-28 23:44:31 +0100

    Have working speach bubble class now

--- src/bubble.js
+++ src/bubble.js
@@ -1,20 +1,50 @@
 import Phaser from 'phaser'
 
 export default class Bubble extends Phaser.Group {
-    constructor(game, x, y, type, w=50, h=150, text= "Croatoan!") {
+
+    constructor(game, x, y, flipped = false) {
 	super(game)
-	var sprite = this.create(0, 0, 'thought_bubble');
-	if(type == "Speach")
-	    create_speach_bubble()
-	else if(type == "Thought")
-	    create_thought_bubble()
+	this.x = x + 60
+	this.y = y - 140
+	this.graphics = game.add.graphics(x, y)
+	this.displayText = this.game.add.text(
+            this.x, this.y,"",
+            {
+                font: '18px Verdana',
+                backgroundColor: 'rgba(255,255,255,0)',
+                wordWrap: true,
+                wordWrapWidth: 250
+            })
     }
 
-    create_speach_bubble() {
-
+    reset(){
+	if(this.graphics)
+	    this.graphics.clear()
+	this.graphics = this.game.add.graphics(0, 0)
+	this.displayText.text = ""
     }
 
-    create_thought_bubble() {
+    create_speach(text) {
+	this.reset()
+	this.displayText.text = text;
+	this.graphics.beginFill(0xFFFFFF)
+	this.graphics.drawRoundedRect( this.x - 10,
+				       this.y - 10,
+				       this.displayText.width+20,
+				       this.displayText.height+10)
+	this.graphics.endFill();
 
+	this.graphics.beginFill(0xFFFFFF)
+	this.graphics.moveTo(this.x + 10, this.y + this.displayText.height)
+	this.graphics.lineTo(this.x - 15, this.y + this.displayText.height+50)
+	this.graphics.lineTo(this.x + 25, this.y + this.displayText.height)
+	this.graphics.endFill();
+
+	this.game.world.bringToTop(this.displayText)
+    }
+
+    create_thought(text) {
+	this.reset()
+	var sprite = this.create(this.x, this.y, 'thought_bubble');
     }
 }
--- src/opponent.js
+++ src/opponent.js
@@ -14,15 +14,14 @@ export default class Opponent extends Phaser.Sprite {
 	this.animations.add('talk', [0, 1], 10, true)
 	this.animations.add('collapse', [2, 3], 10, true)
 
-	this.graphics = this.game.add.graphics(0, 0)
 	this.sanity = 1
+
+	this.bubble = new Bubble(this.game, this.x, this.y, true)
     }
 
     reset() {
-	if (this.displayText) {
-	    this.displayText.destroy()
-	    this.graphics.clear();
-	}
+	this.bubble.reset()
+
 	if(this.sanity < 1)
 	    this.animations.play('collapse')
     }
@@ -45,34 +44,9 @@ export default class Opponent extends Phaser.Sprite {
 	if(allQuestions.length > 0)
 	    question = allQuestions.pop()
 
-	let pos = new Phaser.Point(this.x + 60, this.y - 140 );
-	this.displayText = this.game.add.text(
-            pos.x, pos.y,
-            question,
-            {
-                font: '18px Verdana',
-                backgroundColor: 'rgba(255,255,255,0)',
-                wordWrap: true,
-                wordWrapWidth: 250
-            })
-
-	this.graphics.clear();
-	this.graphics = this.game.add.graphics(0, 0)
-	this.graphics.beginFill(0xFFFFFF)
-	this.graphics.drawRoundedRect( pos.x - 10,
-				       pos.y - 10,
-				       this.displayText.width+20,
-				       this.displayText.height+10)
-	this.graphics.endFill();
-
-	this.graphics.beginFill(0xFFFFFF)
-	this.graphics.moveTo(pos.x + 10, pos.y + this.displayText.height)
-	this.graphics.lineTo(pos.x - 15, pos.y + this.displayText.height+50)
-	this.graphics.lineTo(pos.x + 25, pos.y + this.displayText.height)
-	this.graphics.endFill();
-
-	this.game.world.bringToTop(this.displayText)
 	this.animations.play('talk')
+
+	this.bubble.create_speach(question)
 
 	// Ask question for 2 seconds
 	this.game.time.events.add(Phaser.Timer.SECOND * 2, () => {
