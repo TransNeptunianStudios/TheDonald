5d314ff90ab1080a4a9ad59a9be7b7d292f590ca
Reicher <robin.o.reicher@gmail.com>
2017-04-17 23:54:11 +0200

    Some work with pride-level

--- src/prideparty.js
+++ src/prideparty.js
@@ -17,18 +17,21 @@ export default class PrideParty extends Level {
 
     start() {
 	super.start()
-	this.add_sprite('hanging_lamp', this.game.width*0.3, 50)
+	this.add_sprite('hanging_lamp', this.game.width*0.2, 50)
 	this.add_sprite('hanging_lamp', this.game.width*0.5, 50)
-	this.add_sprite('hanging_lamp', this.game.width*0.7, 50)
+	this.add_sprite('hanging_lamp', this.game.width*0.8, 50)
 
-	this.add_sprite('pride_flag', this.game.width*0.5, 200)
-	this.add_sprite('pride_symbol', 200, 200)
+	this.add_sprite('plant', this.game.width*0.2, 320)
+	this.add_sprite('plant', this.game.width*0.8, 320)
+
+	this.add_sprite('pride_flag', this.game.width*0.5, 190)
+	this.add_sprite('pride_symbol', this.game.width*0.10, 150)
 	this.add_sprite('pride_heart', this.game.width*0.5, 250)
 
 	this.add_animated_loop('pride_kids', 650, 350)
 	this.add_animated_loop('pride_dancer', 350, 310, 3)
 
-	this.kaj = this.backGroup.create(250, 350, 'pride_kaj')
+	this.kaj = this.backGroup.create(250, 300, 'pride_kaj')
 	this.kaj.animations.add('loop_animation')
 	this.game.time.events.loop(Phaser.Timer.SECOND*3, this.updateKaj, this)
     }
